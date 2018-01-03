e42d28f2510f9834278d7be175d29569c20b80d7
Onur <onurburak@sabanciuniv.edu>
2018-01-03 10:43:27 -0800

    comment

--- q2.playground/Contents.swift
+++ q2.playground/Contents.swift
@@ -12,21 +12,37 @@ class Node: Equatable {
         self.next = nil
     }
     static func == (lhs: Node, rhs: Node) -> Bool {
-        return lhs.value == rhs.value && lhs.next == rhs.next
+        return lhs.value == rhs.value
     }
 }
-//Function to detect
+//Function to detect cycle by storing the path in an array and check if it visits the same node twice
 func detectCycle(Node:Node?)->Bool{
     var arrayOfNodes:[Node] = [];
     var currentNode = Node;
     
     while currentNode != nil {
-        if arrayOfNodes.contains(currentNode!) {
-            return true
-        }else{
-            arrayOfNodes.append(currentNode!)
+        for nodes in arrayOfNodes{
+            if nodes === currentNode {
+                return true
+            }
         }
+        arrayOfNodes.append(currentNode!)
         currentNode = currentNode!.next
+    }
+    return false
+}
+//Function to detect cycle by 2 iterator whose iteration speed is different
+func detectCycleEff(Node:Node?)->Bool{
+    var iteratorSlow = Node;
+    var iteratorFast = Node;
+    
+    while iteratorFast != nil && iteratorFast!.next != nil{
+        iteratorSlow = iteratorSlow!.next
+        iteratorFast = iteratorFast!.next!.next
+        
+        if(iteratorFast === iteratorSlow){
+            return true
+        }
     }
     return false
 }
@@ -36,8 +52,8 @@ var secondNode = Node(value: 5)
 var thirdNode = Node(value: 15)
 
 node.next = secondNode
-secondNode.next = node
-
-
+secondNode.next = thirdNode
+thirdNode.next = node
 
 detectCycle(Node: node)
+detectCycleEff(Node: node)
